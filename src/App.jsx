f6607c62d76c2b96c599c65eb4c07c389fa484fc
Bintangawan <bintangawan0418@gmail.com>
2025-11-12 12:12:17 +0700

    feat(): Structure Folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>Hello, Frontend Started Here!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
